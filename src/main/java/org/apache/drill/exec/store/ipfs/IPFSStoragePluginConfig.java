5b39adc208a3af66bcaaa7d28c31b1c10c6a8211
dbw9580 <qc_efd4adf578f34ee18e06216a325d7386@git.code.tencent.com>
2019-03-09 22:17:15 +0800

    Add format plugin support (ongoing).

--- src/main/java/org/apache/drill/exec/store/ipfs/IPFSStoragePluginConfig.java
+++ src/main/java/org/apache/drill/exec/store/ipfs/IPFSStoragePluginConfig.java
@@ -1,9 +1,12 @@
 package org.apache.drill.exec.store.ipfs;
 
-import org.apache.drill.common.logical.StoragePluginConfigBase;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.apache.drill.common.logical.FormatPluginConfig;
+import org.apache.drill.common.logical.StoragePluginConfigBase;
+
+import java.util.Map;
 
 @JsonTypeName(IPFSStoragePluginConfig.NAME)
 public class IPFSStoragePluginConfig extends StoragePluginConfigBase{
@@ -20,16 +23,21 @@ public class IPFSStoragePluginConfig extends StoragePluginConfigBase{
     @JsonProperty("ipfs-timeout")
     private final int ipfsTimeout;
 
+    @JsonProperty
+    private final Map<String, FormatPluginConfig> formats;
+
     @JsonCreator
     public IPFSStoragePluginConfig(
         @JsonProperty("host") String host,
         @JsonProperty("port") int port,
         @JsonProperty("max-nodes-per-leaf") int maxNodesPerLeaf,
-        @JsonProperty("ipfs-timeout") int ipfsTimeout) {
+        @JsonProperty("ipfs-timeout") int ipfsTimeout,
+        @JsonProperty("formats") Map<String, FormatPluginConfig> formats) {
         this.host = host;
         this.port = port;
         this.maxNodesPerLeaf = maxNodesPerLeaf;
         this.ipfsTimeout = ipfsTimeout;
+        this.formats = formats;
     }
 
     public String getHost() {
@@ -48,12 +56,17 @@ public class IPFSStoragePluginConfig extends StoragePluginConfigBase{
         return ipfsTimeout;
     }
 
+    public Map<String, FormatPluginConfig> getFormats() {
+        return formats;
+    }
+
     @Override
     public int hashCode() {
         String host_port = String.format("%s:%d[%d,%d]", host, port, maxNodesPerLeaf, ipfsTimeout);
         final int prime = 31;
         int result = 1;
         result = prime * result + ((host_port == null) ? 0 : host_port.hashCode());
+        result = prime * result + ((formats == null) ? 0 : formats.hashCode());
         return result;
     }
 
@@ -69,6 +82,13 @@ public class IPFSStoragePluginConfig extends StoragePluginConfigBase{
             return false;
         }
         IPFSStoragePluginConfig other = (IPFSStoragePluginConfig) obj;
+        if (formats == null) {
+            if (other.formats != null) {
+                return false;
+            }
+        } else if (!formats.equals(other.formats)) {
+            return false;
+        }
         if (host == null) {
             if (other.host != null) {
                 return false;
